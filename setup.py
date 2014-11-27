904e4d4e8d302eee5dec63f1ce68ddb5430b7f50
Francis Tseng <f+accounts@frnsys.com>
2014-11-26 19:08:11 -0500

    renamed to futora, made installable via pip

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='futora',
+    version='0.1.0',
+    description='futora sphinx theme',
+    url='https://github.com/ftzeng/futora.sphinx',
+    author='Francis Tseng',
+    author_email='f@frnsys.com',
+    license='AGPLv3',
+
+    packages=['futora'],
+)
